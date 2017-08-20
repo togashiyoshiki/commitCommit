13cb2e338bd425ed3f31b07028855f2ecfccd9fb
冨樫義紀 <togashiyoshiki@icloud.com>
2017-08-20 19:09:42 +0900

    変更箇所あり。これからtableviewにコアデータを表示する

--- addCommitViewController.swift
+++ addCommitViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
-
-class addCommitViewController: UIViewController {
+import GoogleMobileAds
+class addCommitViewController: UIViewController,GADBannerViewDelegate {
 
     var item:String = ""
+    var tit:String = ""
+    ///じぶんのAdMobIDを指定指定します
+    let AdMobID = "ca-app-pub-3530000000000000/0123456789"
+    let TEST_DEVICE_ID = "61b0154xxxxxxxxxxxxxxxxxxxxxxxe0"
+    ///テスト用
+    let AdMobTest:Bool = true
+    let SimulatorTest:Bool = true
+    
     
     @IBOutlet weak var myCommitLabel: UILabel!
+    @IBOutlet weak var mytit: UILabel!
     
     
     var timer = Timer()                 // Timerクラス
@@ -105,9 +114,57 @@ class addCommitViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        myCommitLabel.text = item
+        mytit.text = tit
+        
+        //広告を表示するメソッドを呼び出す
+        showAdBanner()
+
     }
 
+    //広告を表示するfunc
+    
+    func showAdBanner(){
+        
+        var admobView = GADBannerView()     //グーグルマップが用意しているバナー用のviewです。
+        
+        //今回は下の方にバナーを出します。
+        
+        admobView = GADBannerView(adSize: kGADAdSizeBanner)
+        
+        //バナーを下に配置します。
+        admobView.frame.origin = CGPoint(x: 0, y: self.view.frame.size.height - admobView.frame.height)
+        
+        //バナーを横幅いっぱいに配置します。
+        admobView.frame.size =  CGSize(width: self.view.frame.width, height: admobView.frame.height)
+        
+        //各種設定
+        admobView.adUnitID = AdMobID
+        admobView.delegate = self
+        admobView.rootViewController = self
+        
+        //広告のリクエスト
+        let admobRequest = GADRequest()
+        
+        //テストパターンによって設定を変えます。
+        if (AdMobTest == true){
+            
+            //シュミレーターでのテスト方法
+            if (SimulatorTest == true){
+                admobRequest.testDevices = [kGADSimulatorID]
+                print("Simulator")
+            }else{
+                //実機テスト
+                admobRequest.testDevices = [TEST_DEVICE_ID]
+                print("device")
+            }
+        }
+        
+        //リクエストのロード
+        admobView.load(admobRequest)
+        //バナーを画面に追加
+        self.view.addSubview(admobView)
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- commitCommit/ViewController.swift
+++ commitCommit/ViewController.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import CoreData
 import GoogleMobileAds
 
-class ViewController: UIViewController,GADBannerViewDelegate,UIPickerViewDataSource,UIPickerViewDelegate {
+class ViewController: UIViewController,GADBannerViewDelegate,UITableViewDelegate,UITableViewDataSource {
     
 //    コミットを追加するテキストフィールド
     @IBOutlet weak var addTextField: UITextField!
     
     ///    detaを配列で使用する
     var commitList = ["勉強","仕事","趣味"]
+    var titText:String = ""
+    
+    var arrayList = ["hoge","hgoehgoe","gdsa"]
+    
     
 //    セグエで飛ばすための配列
     var commitSegue:String = "勉強"
@@ -28,60 +33,57 @@ class ViewController: UIViewController,GADBannerViewDelegate,UIPickerViewDataSou
     let AdMobTest:Bool = true
     let SimulatorTest:Bool = true
     
-    
-    //    ピッカービューの列数を決定する。メソッド追加
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-   
-        return 1
-    }
-    //    ピッカービューの行数を決定する。メソッド追加
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        //        配列の要素の個数と同じ数を指定
-                return commitList.count
-    }
-    //    ピッカービューに表示する文字の設定。　メソッド追加
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        
-        //rowの中に配列が入ります。
-        
-        return commitList[row]
-        
-    }
-    //    ピッカービューが選択された時に発動
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        //        ユー ザが選んだ時にデバックエリアに選んだものを表示します。
-        
-        
-        print(commitList[row])
-//        配列を変数に代入
-        commitSegue = commitList[row]
-    }
-    
+         
     
 //    コミットをする
     @IBAction func addCommit(_ sender: UIButton) {
+        if addTextField.text == "" {
+        addTextField.placeholder = "コミットタイトルを入力してください"
+        }else{
          performSegue(withIdentifier: "Segue3", sender: nil)
+        
+        }
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //        guestの中身segueに飛んだSecondViewControllerクラスの継承している
         if (segue.identifier == "Segue3") {
             let guest = segue.destination as! addCommitViewController
+            titText = addTextField.text!
             
             ///項目
             guest.item = commitSegue
-            
+            guest.tit = titText
         }
     }
-    
    
+    //    ⑵行数を決める
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return arrayList.count
+    }
+    //    ⑶リストに表示する文字列を決定し、表示
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell {
+        //        文字を表示するセルの取得(セルの再利用)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        //        表示したい文字の設定
+        
+          cell.textLabel?.text = arrayList[indexPath.row]
+        
+            return cell
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //広告を表示するメソッドを呼び出す
         showAdBanner()
-        
+        read()
     }
+    
+    
     
     //広告を表示するfunc
     
@@ -134,6 +136,39 @@ class ViewController: UIViewController,GADBannerViewDelegate,UIPickerViewDataSou
         // Dispose of any resources that can be recreated.
     }
     
+    
+    //    データを取ります。
+    func read(){
+        //        カラの配列を用意します。
+//        myTitle = []
+//        myDetail = []
+//        myDeta = []
+//        myImage = []
+        //        AppDelegateを使う準備
+        let appDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        //        エンティティを操作するためのオブジェクト
+        let viewContext = appDelegate.persistentContainer.viewContext
+        //        どのエンティティからデータを取得してくるか設定
+        let query: NSFetchRequest<TimeMg> = TimeMg.fetchRequest()
+        
+        do{
+            query.sortDescriptors = [NSSortDescriptor(key: "nowTime",ascending: false)]
+            //            データを一括取得
+            let fetchResults = try viewContext.fetch(query)
+            //            データの取得
+            for result: AnyObject in fetchResults {
+
+                
+                let nowTime :NSDate! = result.value(forKey: "nowTime") as! NSDate
+                let tagetTime = result.value(forKey: "tagetTime")
+                
+                 let str: String = String("\(tagetTime)")
+                
+                arrayList.append(str)
+            }
+        }catch{
+        }
+    }
     
 }
 
